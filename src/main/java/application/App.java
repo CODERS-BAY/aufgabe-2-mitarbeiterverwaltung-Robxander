daf48baa5e8ccadaa6a44d3de08b37153a0a3351
Robert <r.kohl@xentego.at>
2023-10-10 14:56:16 +0200

    Aufgabe erlegit, Refactoring fehlt

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,21 +1,95 @@
 package application;
 
+import java.util.Scanner;
+
 public class App {
-	public static void main(String[] args) {
+    private static int generateId() {
+        return (int) (1000 * Math.random());
+    }
 
-	}
+    static Scanner scanner = new Scanner(System.in);
 
-	/*
-	 * for the bonus task
-	 *
-	 * private static int generateID() {}
-	 *
-	 */
+    static int deptCounter = 0;
+    static int empCounter = 0;
 
-	/*
-	 * for the console application
-	 *
-	 * private static void application() {}
-	 *
-	 */
+    //////////////////////////////////////////////////////////////////////////// MAIN
+    public static void main(String[] args) {
+
+        Department musterDepartment = new Department();
+        Employee musterEmployee = new Employee();
+
+        Department department1 = new Department("Codersbay", generateId(), "Österreich", "Linz");
+        Department department2 = new Department("Magna", generateId(), "Österreich", "Steyr");
+        Department department3 = new Department("NXP", generateId(), "Österreich", "Graz");
+
+        Employee employee1 = new Employee("Franz ", "Mayer", generateId(), musterDepartment);
+        Employee employee2 = new Employee("Hans ", "Mayer", generateId(), department1);
+        Employee employee3 = new Employee("Willi ", "Mayer", generateId(), department1);
+        Employee employee4 = new Employee("Willi ", "Mayer", generateId(), department1);
+
+        musterEmployee.print();
+        employee1.print();
+        employee2.print();
+
+//        System.out.println("Department ist " + department1.isEquals(department3));
+//        System.out.println("Employee ist " + employee1.isEquals(employee2));
+
+        System.out.println("Bitte legen Sie eine Abteilung an.");
+        System.out.print("Name: ");
+        String inputDept = scanner.nextLine();
+        System.out.print("Land: ");
+        String inputLand = scanner.nextLine();
+        System.out.print("Stadt: ");
+        String inputCity = scanner.nextLine();
+
+        Department[] deptArray = new Department[1000];
+        deptArray[deptCounter] = new Department(inputDept, generateId(), inputLand, inputCity);
+
+        char inputRepeat;
+        Employee[] empArray = new Employee[1000];
+        do {
+            System.out.println(" Bitte legen Sie einen Mitarbeiter an.");
+            System.out.print("Vorname: ");
+            String inputFirstName = scanner.nextLine();
+            System.out.print("Nachname: ");
+            String inputLastName = scanner.nextLine();
+
+
+            empArray[empCounter] = new Employee(inputFirstName, inputLastName, generateId(), deptArray[deptCounter]);
+
+
+            System.out.println("Sie haben folgenden Mitarbeiter angelegt: ");
+
+            System.out.println("Der Mitarbeiter " + empArray[empCounter].id + " " + empArray[empCounter].firstName + " "
+                    + empArray[empCounter].lastName + " arbeitet in der Abteilung " + deptArray[deptCounter].name
+                    + " in " + deptArray[deptCounter].city);
+
+            empCounter++;
+
+            System.out.println("Möchten Sie noch einen Mitarbeiter anlegen?");
+            System.out.println("j für ja und n für beenden");
+            inputRepeat = scanner.nextLine().charAt(0);
+        } while (inputRepeat == 'j');
+
+        System.out.println("Folgende Mitarbeiter wurden angelegt:");
+        for(int i = 0; i< empCounter; i++){
+            System.out.println(empArray[i].id + " "+ empArray[i].firstName + " " + empArray[i].lastName);
+        }
+        System.out.println("Auf Wiedersehen.");
+
+    }
+
+    /*
+     * for the bonus task
+     *
+     * private static int generateID() {}
+     *
+     */
+
+    /*
+     * for the console application
+     *
+     * private static void application() {}
+     *
+     */
 }
--- src/main/java/application/Employee.java
+++ src/main/java/application/Employee.java
@@ -1,18 +1,45 @@
 package application;
 
 public class Employee {
-	final Department dept;
+    String firstName;
+    String lastName;
+    int id;
+    final Department dept1;
 
-	/*
-	 *
-	 * Your code comes here. Remember you can overload constructors.
-	 *
-	 */
-	public void print() {
-		System.out.println();
-	}
+    public Employee(String firstName, String lastName, int id, Department dept1) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.dept1 = dept1;
+    }
 
-	public boolean isEquals(Employee other) {
-		return false;
-	}
+    public Employee() {
+        firstName = "Max ";
+        lastName = "Mustermann";
+        id = 123;
+        dept1 = new Department();
+    }
+
+    public void print() {
+        System.out.println("Der Mitarbeiter " + firstName + lastName + " arbeitet in " +
+                "der Abteilung " + dept1.name + " in " + dept1.city);
+    }
+
+    public boolean isEquals(Employee other) {
+        return id == other.id;
+    }
+
+    public boolean equals(Object obj){
+        if (obj == null){
+            return false;
+        }
+        if (this == obj){
+            return true;
+        }
+        if (getClass() != obj.getClass()){
+            return false;
+        }
+        Employee other = (Employee) obj;
+        return firstName == other.firstName;
+    }
 }
